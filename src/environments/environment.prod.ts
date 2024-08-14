866b984ef49e877434803398bff6ff646e6f17ac
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-08-14 08:45:46 +0200

    create environment folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "your-production-api-key",
+        authDomain: "your-production-auth-domain",
+        projectId: "your-production-project-id",
+        storageBucket: "your-production-storage-bucket",
+        messagingSenderId: "your-production-messaging-sender-id",
+        appId: "your-production-app-id"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "your-development-api-key",
+        authDomain: "your-development-auth-domain",
+        projectId: "your-development-project-id",
+        storageBucket: "your-development-storage-bucket",
+        messagingSenderId: "your-development-messaging-sender-id",
+        appId: "your-development-app-id"
+    }
+};
