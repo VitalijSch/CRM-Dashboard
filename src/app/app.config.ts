f5fd95705983e3be1e65d5c48429fb83b1ea91be
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-08-14 08:54:47 +0200

    a

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,16 +7,15 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 import { provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore } from 'firebase/firestore';
 import { provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
 
-const firebaseConfig = environment.firebaseConfig;
+const firebaseConfig = '';
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+// const app: FirebaseApp = initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideFirebaseApp(() => app),
+    // provideFirebaseApp(() => app),
     provideFirestore(() => getFirestore()),
   ]
 };
